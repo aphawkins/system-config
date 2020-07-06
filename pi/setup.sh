f30267ef3afc1464d3e139d939cef1648fb806aa
Andy Hawkins <aph828@gmail.com>
2020-07-06 23:11:44 +0100

    ip aliases ssh

--- pi/setup.sh
+++ pi/setup.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+# Update
 sudo apt update -y
 sudo apt autoremove -y
-sudo apt upgrade -y
+sudo apt full-upgrade -y
+
+# Static IP
+STATICIPFILE=/boot/cmdline.txt
+STATICIP='ip=192.168.2.3'
+sudo grep -qF -- "$STATICIP" "$STATICIPFILE" || echo "$STATICIP" >> "$STATICIPFILE"
+
+# Aliases
+FILE=/home/pi/.bash_aliases
+touch $FILE
+ALIAS='alias la="ls -a"'
+sudo grep -qF -- "$ALIAS" "$FILE" || echo "$ALIAS" >> "$FILE"
+ALIAS='alias ll="ls -la"'
+sudo grep -qF -- "$ALIAS" "$FILE" || echo "$ALIAS" >> "$FILE"
+ALIAS='alias l="ls -CF"'
+sudo grep -qF -- "$ALIAS" "$FILE" || echo "$ALIAS" >> "$FILE"
+
+# ssh key
+SSHPUBFILE=/home/pi/.ssh/id_rsa.pub
+if [ ! -f "$SSHPUBFILE" ]; then
+    ssh-keygen -C "andy@raspberrypi"
+fi
+cat $SSHPUBFILE
 
 # VSCode
 sudo -s
@@ -17,9 +40,8 @@ echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sour
 sudo apt-get update
 sudo apt-get install plexmediaserver
 
-
 # NAS Mounts
 sudo mkdir /mnt/archer
-sudo mount -t cifs -o vers=1.0 "//192.168.2.2/sdb1(sdb1)/MEDIA-DAD/Dad" /mnt/archer
+sudo mount -t cifs -o vers=1.0 "//192.168.2.2/sdb1(sdb1)/MEDIA-DAD/Dad" /mnt/media-dad
 sudo mkdir /mnt/media
-sudo mount -t cifs -o vers=1.0 "//192.168.2.2/sda1(sda1)/MEDIA" /mnt/media
+sudo mount -t cifs -o vers=1.0 "//192.168.2.2/sda1(sda1)/MEDIA" /mnt/media-kids
